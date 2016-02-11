9b984f40e75bfc1512d4fb22890a9e37c1932649
Toni Lappalainen <Toni.Lappalainen@landisgyr.com>
2016-02-11 20:41:55 +0200

    Add ability to search for content. Refactor main.py to conform to Python coding style. Debug logging can be turned on optionally through settings.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import json
 import base64
 from Crypto.Cipher import AES
 from urlparse import parse_qsl
+import xbmc
+import xbmcaddon
 import xbmcgui
 import xbmcplugin
 import credentials
@@ -18,10 +20,26 @@ _url = sys.argv[0]
 # Get the plugin handle as an integer number.
 _handle = int(sys.argv[1])
 
-_appId = credentials._appId
-_appKey = credentials._appKey
-_secretKey = credentials._secretKey
+_app_id = credentials._appId
+_app_key = credentials._appKey
+_secret_key = credentials._secretKey
 _unplayableCategories = ["5-162", "5-164"]
+_addonid = 'plugin.video.areena'
+_addon = xbmcaddon.Addon(id=_addonid)
+
+
+def log(txt, log_level=xbmc.LOGDEBUG):
+    """
+    Log something to the kodi.log file
+    :param txt: Text to write to the log
+    :param log_level: Severity of the log text
+    :return: None
+    """
+    if (_addon.getSetting("debug") == "true") or (log_level != xbmc.LOGDEBUG):
+        if isinstance(txt, str):
+            txt = txt.decode("utf-8")
+        message = u'%s: %s' % (_addonid, txt)
+        xbmc.log(msg=message.encode("utf-8"), level=log_level)
 
 
 def get_categories():
@@ -32,13 +50,8 @@ def get_categories():
     from some site or server.
     :return: list
     """
-    url = "https://external.api.yle.fi/v1/programs/categories.json?app_id=" + _appId + "&app_key=" + _appKey
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-
-    #print(data['data'])
-
-    return data['data']
+    url = "https://external.api.yle.fi/v1/programs/categories.json?app_id=" + _app_id + "&app_key=" + _app_key
+    return get_json(url)['data']
 
 
 def get_videos(category, offset):
@@ -46,8 +59,9 @@ def get_videos(category, offset):
     Get the list of videofiles/streams.
     Here you can insert some parsing code that retrieves
     the list of videostreams in a given category from some site or server.
-    :param category: str
-    :return: list
+    :param category: category id
+    :param offset: offset for videos to retrieve
+    :return: json data of the videos
     """
 
     url = "https://external.api.yle.fi/v1/programs/items.json?" \
@@ -57,13 +71,8 @@ def get_videos(category, offset):
           "&order=updated:desc" \
           "&contentprotection=22-0,22-1" \
           "&offset=" + str(offset) + \
-          "&app_id=" + _appId + "&app_key=" + _appKey
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
-
-    #print(data['data'])
-
-    return data['data']
+          "&app_id=" + _app_id + "&app_key=" + _app_key
+    return get_json(url)['data']
 
 
 def list_categories():
@@ -75,33 +84,37 @@ def list_categories():
     categories = get_categories()
     # Create a list for our items.
     listing = []
+    # Add static "categories"
+    search_list_item = xbmcgui.ListItem(label='[Haku]')
+    search_url = '{0}?action=search'.format(_url)
+    listing.append((search_url, search_list_item, True))
     # Iterate through categories
     for category in categories:
         if 'broader' not in category:
-          continue
+            continue
         if 'id' not in category['broader']:
-          continue
+            continue
         if category['id'] in _unplayableCategories:
-          continue
+            continue
 
         if category['broader']['id'] == '5-130':
-          # Create a list item with a text label and a thumbnail image.
-          list_item = xbmcgui.ListItem(label=category['title']['fi'])
-          # Set a fanart image for the list item.
-          # Here we use the same image as the thumbnail for simplicity's sake.
-          #list_item.setProperty('fanart_image', VIDEOS[category][0]['thumb'])
-          # Set additional info for the list item.
-          # Here we use a category name for both properties for for simplicity's sake.
-          # setInfo allows to set various information for an item.
-          # For available properties see the following link:
-          # http://mirrors.xbmc.org/docs/python-docs/15.x-isengard/xbmcgui.html#ListItem-setInfo
-          list_item.setInfo('video', {'title': category['title']['fi'], 'genre': category['type']})
-          # Create a URL for the plugin recursive callback.
-          url = '{0}?action=listing&category={1}'.format(_url, category['id'])
-          # is_folder = True means that this item opens a sub-list of lower level items.
-          is_folder = True
-          # Add our item to the listing as a 3-element tuple.
-          listing.append((url, list_item, is_folder))
+            # Create a list item with a text label and a thumbnail image.
+            list_item = xbmcgui.ListItem(label=category['title']['fi'])
+            # Set a fanart image for the list item.
+            # Here we use the same image as the thumbnail for simplicity's sake.
+            # list_item.setProperty('fanart_image', VIDEOS[category][0]['thumb'])
+            # Set additional info for the list item.
+            # Here we use a category name for both properties for for simplicity's sake.
+            # setInfo allows to set various information for an item.
+            # For available properties see the following link:
+            # http://mirrors.xbmc.org/docs/python-docs/15.x-isengard/xbmcgui.html#ListItem-setInfo
+            list_item.setInfo('video', {'title': category['title']['fi'], 'genre': category['type']})
+            # Create a URL for the plugin recursive callback.
+            url = '{0}?action=listing&category={1}'.format(_url, category['id'])
+            # is_folder = True means that this item opens a sub-list of lower level items.
+            is_folder = True
+            # Add our item to the listing as a 3-element tuple.
+            listing.append((url, list_item, is_folder))
     # Add our listing to Kodi.
     # Large lists and/or slower systems benefit from adding all items at once via addDirectoryItems
     # instead of adding one by ove via addDirectoryItem.
@@ -112,41 +125,41 @@ def list_categories():
     xbmcplugin.endOfDirectory(_handle)
 
 
-def list_videos(category, offset):
+def list_videos(videos, offset_url):
     """
     Create the list of playable videos in the Kodi interface.
-    :param category: str
+    :param videos: json of videos to list
+    :param offset_url: url that opens next page of videos
     :return: None
     """
-    # Get the list of videos in the category.
-    videos = get_videos(category, offset)
     # Create a list for our items.
     listing = []
-    #list.append(('{0}', '...', True))
+    # list.append(('{0}', '...', True))
     # Iterate through videos.
     for video in videos:
         # Create a list item with a text label and a thumbnail image.
         if 'fi' in video['title']:
-          list_item = xbmcgui.ListItem(label=video['title']['fi'])
+            list_item = xbmcgui.ListItem(label=video['title']['fi'])
         else:
-          print('no finnish title for video')
+            log('no finnish title for video: {}'.format(video['title']), xbmc.LOGWARNING)
 
         # Set a fanart image for the list item.
         # Here we use the same image as the thumbnail for simplicity's sake.
-        #list_item.setProperty('fanart_image', video['thumb'])
+        # list_item.setProperty('fanart_image', video['thumb'])
         # Set additional info for the list item.
-        #if 'fi' in video['description']:
+        # if 'fi' in video['description']:
         #  list_item.setInfo('video', {'title': video['description']['fi']})
         # Set additional graphics (banner, poster, landscape etc.) for the list item.
         # Again, here we use the same image as the thumbnail for simplicity's sake.
 
         if 'available' in video['image']:
-          #print("Available field exists")
-          if video['image']['available']:
-            imageUrl = 'http://images.cdn.yle.fi/image/upload/w_240,h_240,c_fit/{0}.png'.format(video['image']['id'])
-            #print("Image url is " + imageUrl)
-            #list_item.setArt({'landscape': imageUrl})
-            list_item.setThumbnailImage(imageUrl)
+            # log("Available field exists")
+            if video['image']['available']:
+                image_url = 'http://images.cdn.yle.fi/image/upload/w_240,h_240,c_fit/{0}.png'.format(
+                    video['image']['id'])
+                # log("Image url is " + imageUrl)
+                # list_item.setArt({'landscape': imageUrl})
+                list_item.setThumbnailImage(image_url)
         # Set 'IsPlayable' property to 'true'.
         # This is mandatory for playable items!
         list_item.setProperty('IsPlayable', 'true')
@@ -159,89 +172,159 @@ def list_videos(category, offset):
         # Add our item to the listing as a 3-element tuple.
         listing.append((url, list_item, is_folder))
 
-    list_item = xbmcgui.ListItem(label="Next page")
-    url = '{0}?action=listing&category={1}&offset={2}'.format(_url, category, (offset + 25))
-    listing.append((url, list_item, True))
+    if len(listing) > 24:
+        list_item = xbmcgui.ListItem(label="Next page")
+        listing.append((offset_url, list_item, True))
     # Add our listing to Kodi.
     # Large lists and/or slower systems benefit from adding all items at once via addDirectoryItems
     # instead of adding one by ove via addDirectoryItem.
     xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
     # Add a sort method for the virtual folder items (alphabetically, ignore articles)
-    #xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     # Finish creating a virtual folder.
     xbmcplugin.endOfDirectory(_handle)
+    xbmcplugin.setContent(_handle, 'movies')
 
 
 def play_video(path):
-    print(path)
-
-    url = "https://external.api.yle.fi/v1/programs/items/" + path + ".json?app_id=" + _appId + "&app_key=" + _appKey
-    response = urllib.urlopen(url)
-    data = json.loads(response.read())
+    """
+    Play a video by the provided path.
+    :param path: video id
+    :return: None
+    """
+    url = "https://external.api.yle.fi/v1/programs/items/" + path + ".json?app_id=" + _app_id + "&app_key=" + _app_key
+    data = get_json(url)
 
     for publication in data['data']['publicationEvent']:
-        print publication['temporalStatus']
+        log(publication['temporalStatus'])
         if publication['temporalStatus'] == 'currently':
-          print("Found correct publication, media id: " + publication['media']['id'])
-          url = "https://external.api.yle.fi/v1/media/playouts.json?" \
+            log("Found correct publication, media id: " + publication['media']['id'])
+            url = "https://external.api.yle.fi/v1/media/playouts.json?" \
                   "program_id=" + path + \
                   "&media_id=" + publication['media']['id'] + \
                   "&hardsubtitles=true" \
-                  "&protocol=HLS&app_id=" + _appId + \
-                  "&app_key=" + _appKey
-          print(url)
-          response = urllib.urlopen(url)
-          playoutData = json.loads(response.read())
-          #print(playoutData)
-          encryptedUrl = playoutData['data'][0]['url']
-          path = decryptUrl(encryptedUrl)
-
-    """
-    Play a video by the provided path.
-    :param path: str
-    :return: None
-    """
-    print("decrypted path: " + path)
+                  "&protocol=HLS&app_id=" + _app_id + \
+                  "&app_key=" + _app_key
+            playout_data = get_json(url)
+            encrypted_url = playout_data['data'][0]['url']
+            path = decrypt_url(encrypted_url)
+    log("decrypted path: " + path)
     # Create a playable item with a path to play.
     play_item = xbmcgui.ListItem(path=path)
     # Pass the item to the Kodi player.
     xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
 
 
-def decryptUrl(encryptedUrl):
-    return decrypt(encryptedUrl, _secretKey)
+def search(search_string=None, offset=0, clear_search=False):
+    """
+    Manage the search view.
+    :param search_string: string to search
+    :param offset: offset of the results
+    :param clear_search: if true, will clear earlier searches
+    :return: None
+    """
+    if clear_search:
+        _addon.setSetting("searches", "")
+    if search_string is None:
+        log("Show search UI")
+        listing = []
+        new_search_list_item = xbmcgui.ListItem(label='[Uusi haku]')
+        new_search_url = '{0}?action=new_search'.format(_url)
+        listing.append((new_search_url, new_search_list_item, True))
+        clear_search_list_item = xbmcgui.ListItem(label='[Tyhjennä vanhat haut]')
+        clear_search_url = '{0}?action=search&clear_search=1'.format(_url)
+        listing.append((clear_search_url, clear_search_list_item, True))
+        searches = _addon.getSetting("searches").splitlines()
+        for search_item in searches:
+            search_list_item = xbmcgui.ListItem(label=search_item)
+            search_url = '{0}?action=search&search_string={1}'.format(_url, search_item)
+            listing.append((search_url, search_list_item, True))
+        xbmcplugin.addDirectoryItems(_handle, listing, len(listing))
+    else:
+        result = get_json(
+            "https://external.api.yle.fi/v1/programs/items.json?q={0}&offset={1}&order=publication.endtime:desc"
+            "&availability=ondemand&app_id={2}&app_key={3}".format(search_string, offset, _app_id, _app_key))
+        search_url = '{0}?action=search&search_string={1}&offset={2}'.format(_url, search_string, offset + 25)
+        list_videos(result['data'], search_url)
+    xbmcplugin.endOfDirectory(_handle)
 
-def decrypt( enc, key ):
-        enc = base64.b64decode(enc)
-        iv = enc[:16]
-        cipher = AES.new(key, AES.MODE_CBC, iv )
-        #return cipher.decrypt( enc[16:] )
-        return _unpad(cipher.decrypt( enc[16:] ))
 
-def _unpad(s):
-        return s[:-ord(s[len(s)-1:])]
+def new_search():
+    """
+    Creates a new search. Saves it to add-on settings
+    :return: None
+    """
+    keyboard = xbmc.Keyboard()
+    keyboard.doModal()
+    if keyboard.isConfirmed() and keyboard.getText() != '':
+        search_text = keyboard.getText()
+        log(search_text)
+        searches = _addon.getSetting("searches").splitlines()
+        searches.insert(0, search_text)
+        if len(searches) > 20:
+            searches.pop()
+        _addon.setSetting("searches", "\n".join(searches))
+        search(search_text, 0)
+    xbmcplugin.endOfDirectory(_handle)
 
-def router(paramstring):
+
+def decrypt_url(encrypted_url):
+    enc = base64.b64decode(encrypted_url)
+    iv = enc[:16]
+    cipher = AES.new(_secret_key, AES.MODE_CBC, iv)
+    decrypted_url = cipher.decrypt(enc[16:])
+    unpadded_url = decrypted_url[:-ord(decrypted_url[len(decrypted_url)-1:])]
+    return unpadded_url
+
+
+def get_json(url):
+    log(url)
+    response = urllib.urlopen(url)
+    log(response)
+    data = json.loads(response.read())
+    log(data)
+    return data
+
+
+def router(param_string):
     """
     Router function that calls other functions
-    depending on the provided paramstring
-    :param paramstring:
+    depending on the provided param_string
+    :param param_string:
     :return:
     """
-    # Parse a URL-encoded paramstring to the dictionary of
+    # Parse a URL-encoded param_string to the dictionary of
     # {<parameter>: <value>} elements
-    params = dict(parse_qsl(paramstring))
+    params = dict(parse_qsl(param_string))
+    log(params)
+
     # Check the parameters passed to the plugin
     if params:
+        offset = 0
+        if 'offset' in params:
+            offset = int(params['offset'])
         if params['action'] == 'listing':
-            offset = 0;
-            if 'offset' in params:
-              offset = int(params['offset'])
             # Display the list of videos in a provided category.
-            list_videos(params['category'], offset)
+            videos = get_videos(params['category'], offset)
+            url = '{0}?action=listing&category={1}&offset={2}'.format(_url, params['category'], (offset + 25))
+            list_videos(videos, url)
         elif params['action'] == 'play':
             # Play a video from a provided URL.
             play_video(params['video'])
+        elif params['action'] == 'search':
+            search_string = None
+            if 'search_string' in params:
+                search_string = str(params['search_string'])
+            clear_search = False
+            if 'clear_search' in params:
+                clear_search = bool(params['clear_search'])
+            # Show search window
+            search(search_string=search_string, offset=offset, clear_search=clear_search)
+        elif params['action'] == 'new_search':
+            # Show search window
+            new_search()
+        else:
+            log("Unknown action: {0}".format(params['action']), xbmc.LOGERROR)
     else:
         # If the plugin is called from Kodi UI without any parameters,
         # display the list of video categories
